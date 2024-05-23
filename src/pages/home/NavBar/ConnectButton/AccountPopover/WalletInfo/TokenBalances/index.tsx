9263254b37d903269bd00fb760fed9fedd1a1aec
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2024-05-23 12:38:15 +0700

    improve fetching balances

--- src/pages/home/NavBar/ConnectButton/AccountPopover/WalletInfo/TokenBalances/index.tsx
+++ src/pages/home/NavBar/ConnectButton/AccountPopover/WalletInfo/TokenBalances/index.tsx
@@ -1,6 +1,8 @@
 import { useCurrentAccount, useSuiClientQuery } from "@mysten/dapp-kit";
 import { VList } from "virtua";
 import TokenItem from "./TokenItem";
+import { useMemo } from "react";
+import BigNumber from "bignumber.js";
 
 function TokenBalances() {
   const account = useCurrentAccount();
@@ -14,7 +16,13 @@ function TokenBalances() {
     },
   );
 
-  if (!account || !data) {
+  const balances = useMemo(() => {
+    return data?.filter((token) =>
+      new BigNumber(token.totalBalance).isGreaterThan(0),
+    );
+  }, [data]);
+
+  if (!account || !balances?.length) {
     return null;
   }
 
@@ -23,7 +31,7 @@ function TokenBalances() {
       style={{ height: 160, marginRight: -16, paddingRight: 16 }}
       className="vlist"
     >
-      {data.map((token) => (
+      {balances.map((token) => (
         <TokenItem key={token.coinType} item={token} />
       ))}
     </VList>
