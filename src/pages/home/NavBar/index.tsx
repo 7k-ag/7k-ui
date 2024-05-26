1c8b0ec46c8374af9f1d28c843e58ccd57067636
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2024-05-26 15:59:01 +0700

    add aggregator text

--- src/pages/home/NavBar/index.tsx
+++ src/pages/home/NavBar/index.tsx
@@ -4,7 +4,12 @@ import Logo from "./Logo";
 function NavBar() {
   return (
     <nav className="flex items-center justify-between px-4 py-4 sm:px-8 sm:py-6">
-      <Logo />
+      <div className="flex items-center gap-4">
+        <Logo />
+        <div className="flex items-center px-4 py-3 min-h-10 bg-[#252734] rounded-2xl">
+          <span className="text-xs/none">Aggregator</span>
+        </div>
+      </div>
       <ConnectButton />
     </nav>
   );
